a3c0193dd4e4d55f0e9cb1d1f543cb5156dcc1eb
peter <peterfmajor@gmail.com>
2019-11-22 20:24:01 +0000

    extended image ref cacheing in LoadImageTask to hold all loaded images and added debug messages

--- app/src/main/java/com/starViewer/LoadImageTask.java
+++ app/src/main/java/com/starViewer/LoadImageTask.java
@@ -9,37 +9,42 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoadImageTask extends AsyncTask<AssetManager, Void, Bitmap> {
-    private static final String TAG = "LoadImageTask";
+    private static final String SYNCTAG = "LoadImageTaskSync";
+    private static final String ASYNCTAG = "LoadImageTaskAsync";
 
-    //TODO: expand map caching to hold multiple maps
-    private static WeakReference<Bitmap> lastBitmap = new WeakReference<>(null);
-    private static String lastName;
+
+    private static Map<String,WeakReference<Bitmap>> mapCache = new HashMap<>();
 
     private String imageName;
     BitmapImageReciver parent;
 
     public LoadImageTask(BitmapImageReciver parent, String imageName){
+        Log.d(SYNCTAG, "Async image load called for : " + imageName);
         this.parent = parent;
         this.imageName = imageName;
     }
 
     @Override
     protected Bitmap doInBackground(AssetManager... params) {
+        Log.d(ASYNCTAG, "Image load STARTED for : " + imageName);
         AssetManager assetManager = params[0];
 
-        if (imageName.equals(lastName) && lastBitmap.get() != null) {
-            return lastBitmap.get();
+        if (mapCache.containsKey(imageName) && mapCache.get(imageName).get() != null) {
+            Log.d(ASYNCTAG, "Image load CACHE HIT for : " + imageName);
+            return mapCache.get(imageName).get();
         }
 
         try(InputStream istr = assetManager.open(imageName)) {
             Bitmap b = BitmapFactory.decodeStream(istr);
-            lastBitmap = new WeakReference<>(b);
-            lastName = imageName;
+            mapCache.put(imageName, new WeakReference<>(b));
+            Log.d(ASYNCTAG, "Image load FINISHED for : " + imageName);
             return b;
         } catch (IOException e) {
-            Log.e(TAG, "Could not decode default bitmap: " + e);
+            Log.e(ASYNCTAG, "Could not decode default bitmap: " + e);
             return null;
         }
     }
@@ -47,5 +52,6 @@ public class LoadImageTask extends AsyncTask<AssetManager, Void, Bitmap> {
     @Override
     protected void onPostExecute(Bitmap bitmap){
         parent.LoadImageFinished(imageName, bitmap);
+        Log.d(ASYNCTAG, "Image load RETURNED for : " + imageName);
     }
 }
