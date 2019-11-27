fad8a8d82fb679b768267ca915f1cf4565ce255c
AleBaccin <baccin.alessandro@gmail.com>
2019-11-27 09:06:02 +0000

    added TableLayout

--- app/src/main/java/com/example/skylite/MainActivity.java
+++ app/src/main/java/com/example/skylite/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.skylite.Activities.ActivityTrophy;
 
@@ -13,11 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        switchToTrophyActivity();
+    }
+
+    public void goToActivity(View v){
+        if(v.getId() == R.id.trophiesButton){
+            switchToTrophyActivity();
+        }
+        else if(v.getId() == R.id.wikiButton){
+            switchToScrollingActivity();
+            //TODO
+        }
     }
 
     private void switchToTrophyActivity(){
         Intent intent = new Intent(this, ActivityTrophy.class);
         startActivity(intent);
+    }
+
+    private void switchToScrollingActivity(){
+        //TODO
     }
 }
