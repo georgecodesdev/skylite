7d27f6d63945d89a469508253c39aebfe6ad4ca0
seanLetmon97 <sean.letmon@ucdconnect.ie>
2019-11-29 21:03:45 +0000

    achievement unlock

--- app/src/main/java/com/example/skylite/Activities/ActivityTrophy.java
+++ app/src/main/java/com/example/skylite/Activities/ActivityTrophy.java
@@ -1,5 +1,7 @@
 package com.example.skylite.Activities;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,24 +9,32 @@ import com.example.skylite.Fragments.FragmentTrophy;
 import com.example.skylite.MainActivity;
 import com.example.skylite.R;
 
-public class ActivityTrophy extends AbstractActivityTopBar {
+public class ActivityTrophy extends AppCompatActivity {
 
     // The Fragments we are looking for are going to be defined here
     private FragmentTrophy stargazer;
     private FragmentTrophy astronomer;
+    public static FragmentTrophy researcher;
+
+    boolean achievement_complete =  ConstellationInDepthView.achievement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trophy);
-        initIntent(this);
-        getElementsByID();
+
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        getFragments();
         setFragmentAttributes();
+
+        if(achievement_complete==true){
+            achievement_unlocked();
+        }
     }
 
     @Override
@@ -33,18 +43,23 @@ public class ActivityTrophy extends AbstractActivityTopBar {
         startActivity(intent);
     }
 
-    private void getElementsByID(){
+    // gets the UI elements and maps them to the private variables
+    private void getFragments(){
         stargazer = (FragmentTrophy) getSupportFragmentManager().findFragmentById(R.id.starTrophy);
         astronomer = (FragmentTrophy) getSupportFragmentManager().findFragmentById(R.id.astronomerTrophy);
-        toolbar = findViewById(R.id.toolbar);
-        homeNavigationImage = findViewById(R.id.homeNavigationImage);
-        setSupportActionBar(toolbar);
-        setToolbarActionListener();
+        researcher=(FragmentTrophy) getSupportFragmentManager().findFragmentById(R.id.researcherTrophy);
     }
 
     private void setFragmentAttributes(){
         stargazer.setIdentifiers("stargazer", true);
         astronomer.setIdentifiers("astronomer", false);
+        researcher.setIdentifiers("Constellation researcher", false);
+    }
+
+
+    private void achievement_unlocked(){
+        researcher.toggleCompletion(true);
+        achievement_complete=false;
     }
 
 }
